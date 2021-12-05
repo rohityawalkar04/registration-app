e6c2cea91bfd1db17ba6d4384c8a69b01e42ef67
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2021-12-05 23:52:21 +0530

    Rohit - Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import LogIn from "./components/Login/LogIn";
+import DashBoard from "./components/DashBoard/DashBoard";
+import Register from "./components/Register/Register";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Page404 from "./components/Page404/Page404";
+import AboutApp from "./components/AboutApp/AboutApp";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact component={LogIn} />
+        <Route path="/register" exact component={Register} />
+        <ProtectedRoute exact path="/dashboard" component={DashBoard} />
+        <Route path="*" component={Page404} />
+      </Switch>
+      <AboutApp />
+    </div>
+  );
+}
+
+export default App;
